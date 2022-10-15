542120ab4b30ac524ab74bca03c9d62d75d80c05
Jakub Brych <96384633+brychjakub@users.noreply.github.com>
2022-10-15 11:15:47 +0200

    Add files via upload

---
+++ JPGtoPNGfile.py
@@ -0,0 +1,14 @@
+import sys
+import os
+from PIL import Image
+
+
+# python3 JPGtoPNG.py Pokedex\ new\ --> vezmu jpg a převedu ho do png a uložím do složky new
+
+#check if složka new exists, if not, create it
+
+# loop through Pokedex,
+# convert images to png
+# save to new folder
+
+open(r'C:\Users\brych\Desktop\app', 'x')
